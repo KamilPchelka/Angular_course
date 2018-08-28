89554194608b4c7b7fe24d424e475ad3914b7ed0
Kamil.Pchelka <kamil.pchelka@gmail.com>
2018-08-28 15:39:43 +0200

    First iteration.

---
+++ src/app/components/user/user.component.ts
@@ -0,0 +1,43 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['user.component.css'],
+  styles: [
+      `h2 {
+      color: blue
+    }`
+  ]
+})
+
+export class UserComponent implements OnInit {
+
+  user: User;
+
+  constructor() {
+
+
+  }
+
+  ngOnInit(): void {
+
+    this.user = {
+      firstName: 'John',
+      lastName: 'Doe',
+      age: 30,
+      address: {
+        street: '50 main st',
+        city: 'Boston',
+        state: 'Ma'
+      }
+
+    };
+
+  }
+
+}
+
+
+
+
